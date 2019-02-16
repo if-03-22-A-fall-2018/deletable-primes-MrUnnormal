e632c138c28f2f573375804d710f449228cd8b2e
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-02-16 11:20:37 +0100

    is_prime

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -10,3 +10,29 @@
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+#include "deletable_primes.h"
+#include <math.h>
+
+ bool is_prime(unsigned long n)
+ {
+   bool isPrime = true;
+   for (int i = 2; i < sqrt(n); i++)
+   {
+     isPrime = n % i > 0;
+     if(isPrime == false)
+     {
+       return false;
+     }
+   }
+   return true;
+ }
+
+ unsigned long remove_digit(int index, unsigned long n)
+ {
+   return 0;
+ }
+
+ int get_ways(unsigned long p)
+ {
+   return 0;
+ }
